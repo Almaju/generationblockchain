649ff2868807d7500367c61791af578dd2a3df49
Alexandre <Almaju@users.noreply.github.com>
2017-04-25 11:12:12 +0200

    Update app.js

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -26,7 +26,7 @@ let tokenPrice = null;
 window.voteForCandidate = function(candidate) {
   let candidateName = $("#candidate").val();
   let voteTokens = $("#vote-tokens").val();
-  $("#msg").html("Vote has been submitted. The vote count will increment as soon as the vote is recorded on the blockchain. Please wait.")
+  $("#msg").html("Votre vote a été envoyé. Le compteur va se mettre à jour dès que le block sera parvenu à la blockchain. Veuillez patienter.")
   $("#candidate").val("");
   $("#vote-tokens").val("");
 
@@ -53,7 +53,7 @@ window.voteForCandidate = function(candidate) {
 window.buyTokens = function() {
   let tokensToBuy = $("#buy").val();
   let price = tokensToBuy * tokenPrice;
-  $("#buy-msg").html("Purchase order has been submitted. Please wait.");
+  $("#buy-msg").html("L'ordre d'achat a été transmis. Veuillez patienter.");
   Voting.deployed().then(function(contractInstance) {
     contractInstance.buy({value: web3.toWei(price, 'ether'), from: web3.eth.accounts[0]}).then(function(v) {
       $("#buy-msg").html("");
@@ -69,9 +69,9 @@ window.lookupVoterInfo = function() {
   let address = $("#voter-info").val();
   Voting.deployed().then(function(contractInstance) {
     contractInstance.voterDetails.call(address).then(function(v) {
-      $("#tokens-bought").html("Total Tokens bought: " + v[0].toString());
+      $("#tokens-bought").html("Total des parts achetées : " + v[0].toString());
       let votesPerCandidate = v[1];
-      $("#votes-cast").append("Votes cast per candidate: <br>");
+      $("#votes-cast").append("Votes par candidat : <br>");
       let allCandidates = Object.keys(candidates);
       for(let i=0; i < allCandidates.length; i++) {
         $("#votes-cast").append(allCandidates[i] + ": " + votesPerCandidate[i] + "<br>");
